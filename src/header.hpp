8f03384e782210282c2058028df0a926b479a8da
abolfazlsoltnai <166554379+abolfazlsoltnai@users.noreply.github.com>
2024-12-11 11:47:42 +0330

    Delete src directory

--- src/header.hpp
+++
@@ -1,7 +0,0 @@
-#include<SFML/Graphics.hpp>
-#include<SFML/Window.hpp>
-#include<SFML/System.hpp>
-using namespace sf;
-
-
-void get_image_fit(RenderWindow &window,Texture &backgroundTexture,Sprite &backgroundSprite);
